b30d5adf82df307b378d04f972fe81cd63647612
CabralVinicius <cabral.vm@outlook.com>
2024-02-12 19:18:22 -0300

    [INIT] Configurações iniciais de Projeto

---
+++ HabitsPlus/HabitsPlus/HabitsPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsPlusApp.swift
+//  HabitsPlus
+//
+//  Created by Vinicius Cabral on 12/02/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitsPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
